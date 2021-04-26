e9bc121073538f2ff648cef9ba7dc9468fd9fff0
George Spasov <spasov05203@gmail.com>
2021-04-26 23:31:17 +0300

    Added deployment script

--- deployment/deploy.js
+++ deployment/deploy.js
@@ -1,11 +1,13 @@
 const etherlime = require('etherlime-lib');
-const LimeFactory = require('../build/LimeFactory.json');
+const ethers = require('ethers');
+const PolymorphWithGeneChanger = require('../build/PolymorphWithGeneChanger.json');
 
 
 const deploy = async (network, secret, etherscanApiKey) => {
 
-	const deployer = new etherlime.EtherlimeGanacheDeployer();
-	const result = await deployer.deploy(LimeFactory);
+	const deployer = new etherlime.InfuraPrivateKeyDeployer(secret, network, process.env.API_KEY);
+	deployer.defaultOverrides = { gasLimit: 4700000, gasPrice: 3000000000, etherscanApiKey };
+	const result = await deployer.deployAndVerify("etherscan", PolymorphWithGeneChanger, {}, "Polymorph", "MORPH", "http://www.kekdao.com/", "0xe42682eEa1DFC432C2fF5a779CD1D9a1e1c7f405", ethers.utils.parseEther("0.01"));
 
 };
 
