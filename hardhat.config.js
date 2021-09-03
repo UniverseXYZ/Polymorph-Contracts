886314b3d1053b20b2b7d4b0e10eef7481d69ddc
vikinatora <vikinatora@gmail.com>
2021-09-03 14:44:54 +0300

    added dotenv package

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,8 @@
  require("@nomiclabs/hardhat-waffle");
  require("@nomiclabs/hardhat-etherscan");
  require('hardhat-contract-sizer');
+ require('dotenv').config();
+ 
   module.exports = {
    solidity: {
      compilers: [
