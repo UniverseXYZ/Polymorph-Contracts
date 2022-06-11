6990598596b08ab3fb16f4c5a833ee94cade4820
Stanislav Trenev <stanislav.trenev@limechain.tech>
2022-06-11 12:00:01 +0300

    Reslove audit feedback

--- contracts/base/IPolymorph.sol
+++ contracts/base/IPolymorph.sol
@@ -6,8 +6,6 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 interface IPolymorph is IERC721 {
     function geneOf(uint256 tokenId) external view returns (uint256 gene);
 
-    function lastTokenId() external view returns (uint256 tokenId);
-
     function setBaseURI(string memory _baseURI) external;
 
     function setArweaveAssetsJSON(string memory _arweaveAssetsJSON) external;
